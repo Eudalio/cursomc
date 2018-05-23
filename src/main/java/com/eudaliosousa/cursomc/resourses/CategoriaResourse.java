bdf05ec34b848ee7c8a66dd65e32334c6fbf343e
eudalio <tonnydallios@gmail.com>
2018-05-23 19:20:44 -0300

    Testing the first class domain - Category

--- src/main/java/com/eudaliosousa/cursomc/resourses/CategoriaResourse.java
+++ src/main/java/com/eudaliosousa/cursomc/resourses/CategoriaResourse.java
@@ -1,15 +1,28 @@
 package com.eudaliosousa.cursomc.resourses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eudaliosousa.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResourse {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
