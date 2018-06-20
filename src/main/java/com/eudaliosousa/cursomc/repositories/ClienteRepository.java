951e441c7fbfbbd37b76b71ba58089999d0bfed5
eudalio <tonnydallios@gmail.com>
2018-06-20 10:01:04 -0300

    Cliente, TipoCliente, Telefones e Enderecos

---
+++ src/main/java/com/eudaliosousa/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.eudaliosousa.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eudaliosousa.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
