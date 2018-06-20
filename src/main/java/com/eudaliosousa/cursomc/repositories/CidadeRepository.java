3722ebbd98d7b114a4303261c604c236298f1d87
eudalio <tonnydallios@gmail.com>
2018-06-19 23:28:42 -0300

    Estado e Cidade

---
+++ src/main/java/com/eudaliosousa/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.eudaliosousa.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eudaliosousa.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
---
+++ src/main/java/com/eudaliosousa/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.eudaliosousa.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eudaliosousa.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
