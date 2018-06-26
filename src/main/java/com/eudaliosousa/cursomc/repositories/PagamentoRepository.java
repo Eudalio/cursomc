d51c4e6501e9f0569a1c30121b23d9eadc9952ea
eudalio <tonnydallios@gmail.com>
2018-06-26 17:52:27 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/eudaliosousa/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.eudaliosousa.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eudaliosousa.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+
+}
